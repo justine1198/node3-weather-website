b371f0244caa6065e66256dc3a1ffd62b1f0fdf3
Justine1198 <valientejustineann@git.training>
2021-04-12 16:04:35 +0800

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,11 +13,14 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        ". It is currently  " +
+        body.current.weather_descriptions[0] +
+          ". It is currently  " +
           body.current.temperature +
           " degrees out. It feels like " +
           body.current.feelslike +
-          " degrees out"
+          " degrees out. The humidity is " +
+          body.current.humidity +
+          "%."
       );
     }
   });
